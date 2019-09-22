b6f575cd1a24d4957c16069a0d7134224ba3d767
feelingsunny <liuyang6093@gmail.com>
2019-09-21 21:58:58 -0500

    push

--- StarterCode/Belly_Button_Biodiversity/static/js/app.js
+++ StarterCode/Belly_Button_Biodiversity/static/js/app.js
@@ -1,56 +1,73 @@
 function buildMetadata(sample) {
 
-  // @TODO: Complete the following function that builds the metadata panel
+    // @TODO: Complete the following function that builds the metadata panel
 
-  // Use `d3.json` to fetch the metadata for a sample
-    // Use d3 to select the panel with id of `#sample-metadata`
+    // Use `d3.json` to fetch the metadata for a sample
+    var url = `/metadata/${sample}`;
 
-    // Use `.html("") to clear any existing metadata
+    d3.json(url).then(function(sampleData) {
+        console.log(sampleData);
 
-    // Use `Object.entries` to add each key and value pair to the panel
-    // Hint: Inside the loop, you will need to use d3 to append new
-    // tags for each key-value in the metadata.
+        // Use d3 to select the panel with id of `#sample-metadata`
+        var sample_metadata = d3.select("#sample-metadata");
 
-    // BONUS: Build the Gauge Chart
-    // buildGauge(data.WFREQ);
-}
+        // Use `.html("") to clear any existing metadata
+        sample_metadata.html("");
+
+        // Use `Object.entries` to add each key and value pair to the panel
+        // Hint: Inside the loop, you will need to use d3 to append new
+        // tags for each key-value in the metadata.
+
+        Object.entries(sampleData).forEach(([key, value]) => {
+            sample_metadata.append('p').text(`${key}, ${value}`);
+        });
+
+        // BONUS: Build the Gauge Chart
+        // buildGauge(data.WFREQ);
+
+    })
+};
+
 
 function buildCharts(sample) {
 
-  // @TODO: Use `d3.json` to fetch the sample data for the plots
+    // @TODO: Use `d3.json` to fetch the sample data for the plots
+    d3.json(`/samples/${sample}`).then(function(sampleData) {
+        console.log(sampleData);
 
-    // @TODO: Build a Bubble Chart using the sample data
+        // @TODO: Build a Bubble Chart using the sample data
 
-    // @TODO: Build a Pie Chart
-    // HINT: You will need to use slice() to grab the top 10 sample_values,
-    // otu_ids, and labels (10 each).
-}
-
-function init() {
-  // Grab a reference to the dropdown select element
-  var selector = d3.select("#selDataset");
-
-  // Use the list of sample names to populate the select options
-  d3.json("/names").then((sampleNames) => {
-    sampleNames.forEach((sample) => {
-      selector
-        .append("option")
-        .text(sample)
-        .property("value", sample);
+        // @TODO: Build a Pie Chart
+        // HINT: You will need to use slice() to grab the top 10 sample_values,
+        // otu_ids, and labels (10 each).
     });
 
-    // Use the first sample from the list to build the initial plots
-    const firstSample = sampleNames[0];
-    buildCharts(firstSample);
-    buildMetadata(firstSample);
-  });
-}
+    function init() {
+        // Grab a reference to the dropdown select element
+        var selector = d3.select("#selDataset");
 
-function optionChanged(newSample) {
-  // Fetch new data each time a new sample is selected
-  buildCharts(newSample);
-  buildMetadata(newSample);
-}
+        // Use the list of sample names to populate the select options
+        d3.json("/names").then((sampleNames) => {
+            sampleNames.forEach((sample) => {
+                selector
+                    .append("option")
+                    .text(sample)
+                    .property("value", sample);
+            });
 
-// Initialize the dashboard
-init();
+            // Use the first sample from the list to build the initial plots
+            const firstSample = sampleNames[0];
+            buildCharts(firstSample);
+            buildMetadata(firstSample);
+        });
+    }
+
+    function optionChanged(newSample) {
+        // Fetch new data each time a new sample is selected
+        buildCharts(newSample);
+        buildMetadata(newSample);
+    }
+
+    // Initialize the dashboard
+    init();
+}
